8c6e56a55593ecc3a27111bf65c7a41e77c38986
vgaddavcg <vgadda@meetveracity.com>
2017-08-09 13:48:54 -0400

    updated for autoconfigure/starter

---
+++ ascent-platform-starters/ascent-platform-starter-security/src/main/java/gov/va/ascent/starter/security/autoconfigure/AscentSecurityAutoConfiguration.java
@@ -0,0 +1,128 @@
+package gov.va.ascent.starter.security.autoconfigure;
+
+
+import gov.va.ascent.security.TokenResource;
+import gov.va.ascent.security.handler.JwtAuthenticationEntryPoint;
+import gov.va.ascent.security.handler.JwtAuthenticationSuccessHandler;
+import gov.va.ascent.security.jwt.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by vgadda on 5/4/17.
+ */
+
+@Configuration
+@AutoConfigureAfter(SecurityAutoConfiguration.class)
+@EnableConfigurationProperties(JwtAuthenticationProperties.class)
+public class AscentSecurityAutoConfiguration {
+
+    @Configuration
+    @ConditionalOnProperty(prefix = "ascent.security.jwt", name = "enabled", matchIfMissing = true)
+    @Order(JwtAuthenticationProperties.AUTH_ORDER)
+    protected static class JwtWebSecurityConfigurerAdapter
+            extends WebSecurityConfigurerAdapter {
+        @Autowired
+        private JwtAuthenticationProperties jwtAuthenticationProperties;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("/token**").permitAll()
+                    .antMatchers(jwtAuthenticationProperties.getFilterProcessUrl()).authenticated()
+                    .and()
+                    .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().csrf().disable();
+            http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+            http.headers().cacheControl();
+        }
+
+        @Bean
+        protected AuthenticationEntryPoint authenticationEntryPoint(){
+            return new JwtAuthenticationEntryPoint();
+        }
+
+        @Bean
+        protected AuthenticationProvider jwtAuthenticationProvider(){
+            return new JwtAuthenticationProvider(new JwtParser(jwtAuthenticationProperties));
+        }
+
+        @Bean
+        protected AuthenticationSuccessHandler jwtAuthenticationSuccessHandler(){
+            return new JwtAuthenticationSuccessHandler();
+        }
+
+        @Bean
+        protected JwtAuthenticationFilter jwtAuthenticationFilter(){
+            return  new JwtAuthenticationFilter(jwtAuthenticationProperties, jwtAuthenticationSuccessHandler(), jwtAuthenticationProvider());
+        }
+    }
+
+    @Configuration
+    @ConditionalOnProperty(prefix = "ascent.security.jwt", name = "enabled", havingValue = "false")
+    @Order(JwtAuthenticationProperties.AUTH_ORDER)
+    protected static class JwtNoWebSecurityConfigurerAdapter
+            extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private JwtAuthenticationProperties jwtAuthenticationProperties;
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers(jwtAuthenticationProperties.getFilterProcessUrl());
+        }
+
+    }
+
+    @Configuration
+    @Order(JwtAuthenticationProperties.NO_AUTH_ORDER)
+    protected static class NoWebSecurityConfigurerAdapter
+            extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private JwtAuthenticationProperties jwtAuthenticationProperties;
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers(jwtAuthenticationProperties.getExcludeUrls());
+        }
+
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JwtAuthenticationProperties jwtAuthenticationProperties(){
+        return new JwtAuthenticationProperties();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JwtTokenService jwtTokenService(){
+        return  new JwtTokenService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "ascent.security.jwt", name = "enabled", matchIfMissing = true)
+    public TokenResource tokenResource(){
+        return new TokenResource();
+    }
+}
+
+
---
+++ ascent-platform-starters/ascent-platform-starter-swagger/src/main/java/gov/va/ascent/starter/swagger/autoconfigure/SwaggerAutoConfiguration.java
@@ -0,0 +1,112 @@
+package gov.va.ascent.starter.swagger.autoconfigure;
+
+import com.fasterxml.classmate.TypeResolver;
+import gov.va.ascent.framework.service.ServiceResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by vgadda on 8/3/17.
+ */
+
+@Configuration
+@EnableConfigurationProperties(SwaggerProperties.class)
+@EnableSwagger2
+@ConditionalOnProperty(prefix = "ascent.swagger", name = "enabled", matchIfMissing = true)
+@Import({BeanValidatorPluginsConfiguration.class})
+public class SwaggerAutoConfiguration {
+
+    private static final String MESSAGE_200 = "A Response which indicates a successful Request.  The Response may contain \"messages\" that could describe warnings or further information.";
+    private static final String MESSAGE_403 = "The request is not authorized.  Please verify credentials used in the request.";
+    private static final String MESSAGE_400 = "There was an error encountered processing the Request.  Response will contain a  \"messages\" element that will provide further information on the error.  This request shouldn\'t be retried until corrected.";
+    private static final String MESSAGE_500 = "There was an error encountered processing the Request.  Response will contain a  \"messages\" element that will provide further information on the error.  Please retry.  If problem persists, please contact support with a copy of the Response.";
+
+    @Autowired
+    private SwaggerProperties swaggerProperties;
+
+    @Autowired
+    private TypeResolver typeResolver;
+
+    @Bean
+    public Docket categoryApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(swaggerProperties.getGroupName())
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build()
+                .ignoredParameterTypes(ApiIgnore.class)
+                .additionalModels(typeResolver.resolve(ServiceResponse.class))
+                .globalResponseMessage(RequestMethod.GET, globalResponseMessages())
+                .globalResponseMessage(RequestMethod.POST, globalResponseMessages())
+                .globalResponseMessage(RequestMethod.DELETE, globalResponseMessages())
+                .enableUrlTemplating(true)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .version(swaggerProperties.getVersion())
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(swaggerProperties.getSecurePaths()))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+
+        List<SecurityReference> list = new ArrayList<>();
+        list.add(new SecurityReference("Authorization", authorizationScopes));
+        return list;
+    }
+
+    private List<ResponseMessage> globalResponseMessages(){
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(200).message(MESSAGE_200).
+                responseModel(new ModelRef("ServiceResponse")).build());
+        responseMessages.add(new ResponseMessageBuilder().code(400).message(MESSAGE_400).
+                responseModel(new ModelRef("ServiceResponse")).build());
+        responseMessages.add(new ResponseMessageBuilder().code(500).message(MESSAGE_500).
+                responseModel(new ModelRef("ServiceResponse")).build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message(MESSAGE_403).
+                responseModel(new ModelRef("ServiceResponse")).build());
+        return responseMessages;
+    }
+}
