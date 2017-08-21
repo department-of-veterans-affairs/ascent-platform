969f470fe98522b31335ef9918a6c83e8163ed0e
prashanthmds <prashanthmds@gmail.com>
2017-08-21 14:16:34 +0200

    st1015:Docker compose changes for vault

--- ascent-platform-docker-build/stop-all.sh
+++ ascent-platform-docker-build/stop-all.sh
@@ -4,4 +4,6 @@ docker-compose -f docker-compose.yml \
 	-f docker-compose.override.yml \
 	-f docker-compose.logging.yml \
 	-f docker-compose.logging.override.yml \
+    -f docker-compose.vault.yml \
+    -f docker-compose.vault.override.yml \
 	down -v
