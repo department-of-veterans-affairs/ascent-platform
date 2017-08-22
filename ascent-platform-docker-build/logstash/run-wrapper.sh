9a54f81902dbbc98c0af6c8388ae1f3c862414c6
jasonluck <jluck@meetveracity.com>
2017-08-22 13:10:32 -0400

    Adding elasticsearch ssl config

--- ascent-platform-docker-build/logstash/run-wrapper.sh
+++ ascent-platform-docker-build/logstash/run-wrapper.sh
@@ -2,9 +2,12 @@
 
 ENVCONSUL_CONFIG="/usr/share/logstash/template/envconsul-config.hcl"
 CONSUL_TEMPLATE_CONFIG="/usr/share/logstash/template/consul-template-config.hcl"
-SSL_CONFIG="/usr/share/logstash/template/filebeat.ssl.conf"
-NO_SSL_CONFIG="/usr/share/logstash/template/filebeat.conf"
+FILEBEAT_SSL="/usr/share/logstash/template/filebeat.ssl.conf"
+FILEBEAT="/usr/share/logstash/template/filebeat.conf"
 FILEBEAT_CONFIG="/usr/share/logstash/pipeline/filebeat.conf"
+ES_SSL="/usr/share/logstash/template/elasticsearch.ssl.conf"
+ES="/usr/share/logstash/template/elasticsearch.conf"
+ES_CONFIG="/usr/share/logstash/pipeline/elasticsearch.conf"
 CMD="/usr/local/bin/docker-entrypoint"
 
 
@@ -15,12 +18,14 @@ fi
 # If ENVCONSUL_CONFIG is set then run under envconsul to provide secrets in env vars to the process
 if [[ $VAULT_TOKEN ]]; then
     #Use the SSL config
-    cp $SSL_CONFIG $FILEBEAT_CONFIG
+    cp $FILEBEAT_SSL $FILEBEAT_CONFIG
+    cp $ES_SSL $ES_CONFIG
 
     consul-template -once -config="$CONSUL_TEMPLATE_CONFIG" -vault-addr="$VAULT_ADDR"
     envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" $CMD "$@"
 else
     #Use the non SSL config
-    cp $NO_SSL_CONFIG $FILEBEAT_CONFIG
+    cp $FILEBEAT $FILEBEAT_CONFIG
+    cp $ES $ES_CONFIG
     $CMD "$@"
 fi
