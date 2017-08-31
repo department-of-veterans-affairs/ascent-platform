9e1dc415658e9ed5cdb987b28bb0c2fb5fca4c2f
prashanthmds <prashanthmds@gmail.com>
2017-08-31 20:18:03 +0200

    st1015:Changes to vault start up in dev server mode with secrets populated

--- ascent-platform-docker-build/start-vault.sh
+++ ascent-platform-docker-build/start-vault.sh
@@ -1,5 +1,17 @@
 # Starts the vault container for Ascent platform
+#!/bin/sh
 
-docker-compose -f docker-compose.vault.yml \
-	-f docker-compose.vault.override.yml \
-	up --build -d
+VAULT_CONFIG_DIR=./vault/config
+VAULT_TOKEN="${VAULT_TOKEN:-vaultroot}"
+export VAULT_TOKEN=$VAULT_TOKEN
+
+#bring up the docker container for vault
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml up --build -d
+
+
+sleep 1 # wait for Vault to come up
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml exec vault vault auth $VAULT_TOKEN
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml exec vault vault write secret/application ascent.security.jwt.secret=vetsGov
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml exec vault vault write secret/application/docker-demo spring.rabbitmq.password=guest
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml exec vault vault write secret/ascent-demo-service/docker-demo asecnt-demo-service.password=guest
+docker-compose -f docker-compose.vault.yml -f docker-compose.vault.override.yml exec vault vault write secret/ascent-document-service/docker-demo asecnt-document-service.password=guest
--- ascent-platform-docker-build/stop-vault.sh
+++
@@ -1,5 +0,0 @@
-# Stops the vault container
-
-docker-compose -f docker-compose.vault.yml \
-	-f docker-compose.vault.override.yml \
-	down -v
