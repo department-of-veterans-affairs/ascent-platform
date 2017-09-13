dbf1bcfc1642979b58def0834b2ceb376ffd0b03
Nate Paulus <npaulus@meetveracity.com>
2017-09-13 17:44:22 -0500

    Local Sonar Docker Image
    - Docker image to run SonarQube locally
    - Includes Ascent quality profile in the image
    - ascent-platform-parent pom update to include JaCoCo for unit test coverage in SonarQube

---
+++ ascent-platform-docker-build/sonarqube/start_with_profile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+# Start Sonar
+
+./bin/run.sh &
+
+function curlAdmin {
+    curl -v -u admin:admin $@
+}
+
+function isUp {
+    curl -s -u admin:admin -f "$BASE_URL/api/system/info"
+}
+
+# Wait for server to be up
+PING=`isUp`
+while [ -z "$PING" ]
+do
+    sleep 5
+    PING=`isUp`
+done
+
+# Restore qualityprofile
+if [ "$LANGUAGE" ] && [ "$PROFILE_NAME" ]; then
+    curlAdmin -F "backup=@/qualityprofile/java-ascent-32413.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
+    curlAdmin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
+fi
+
+wait
---
+++ ascent-platform-docker-build/sonarqube/stop-sonar.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#Stops sonar cube
+docker-compose -f docker-compose.yml \
+    down
