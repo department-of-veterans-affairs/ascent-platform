447b3ff88395d25450237886e5df287d4bbda63a
jasonluck <jluck@meetveracity.com>
2017-09-19 23:32:11 -0400

    Create base docker image

---
+++ ascent-platform-docker-build/ascent-base/run-wrapper.sh
@@ -0,0 +1,13 @@
+CMD="java -Xms64m -Xmx256m -jar $JAR_FILE"
+
+
+if [[ $VAULT_TOKEN_FILE ]]; then
+    VAULT_TOKEN=$(cat $VAULT_TOKEN_FILE)
+fi
+
+# If VAULT_TOKEN is set then run under envconsul to provide secrets in env vars to the process
+if [[ $VAULT_TOKEN && $VAULT_ADDR ]]; then
+    envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" $CMD
+else
+    $CMD
+fi
