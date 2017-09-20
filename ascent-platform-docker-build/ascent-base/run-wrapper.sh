19a23b6ce26a62073c61777b4e3136109afa96b5
jasonluck <jluck@meetveracity.com>
2017-09-20 15:45:44 -0400

    modify compose file to work with swarm

--- ascent-platform-docker-build/ascent-base/run-wrapper.sh
+++ ascent-platform-docker-build/ascent-base/run-wrapper.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 CMD="java -Xms64m -Xmx256m -jar $JAR_FILE"
 
 
@@ -7,7 +9,7 @@ fi
 
 # If VAULT_TOKEN is set then run under envconsul to provide secrets in env vars to the process
 if [[ $VAULT_TOKEN && $VAULT_ADDR ]]; then
-    envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" $CMD
+    envconsul -config="$ENVCONSUL_CONFIG" -vault-addr=$VAULT_ADDR $CMD
 else
     $CMD
 fi
