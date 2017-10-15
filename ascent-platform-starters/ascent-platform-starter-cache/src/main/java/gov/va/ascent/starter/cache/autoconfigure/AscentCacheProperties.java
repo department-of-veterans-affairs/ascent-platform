ff6f46d935dd4e534e1d6cd3a4344890a79c571f
Abhijit Kulkarni <akulkarni@meetveracity.com>
2017-10-15 15:39:54 -0400

    Clean Up: Removed unused imports from the class

--- ascent-platform-starters/ascent-platform-starter-cache/src/main/java/gov/va/ascent/starter/cache/autoconfigure/AscentCacheProperties.java
+++ ascent-platform-starters/ascent-platform-starter-cache/src/main/java/gov/va/ascent/starter/cache/autoconfigure/AscentCacheProperties.java
@@ -1,16 +1,8 @@
 package gov.va.ascent.starter.cache.autoconfigure;
 
-/**
- * Created by vgadda on 8/11/17.
- */
-import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix="ascent.cache")
 public class AscentCacheProperties {
