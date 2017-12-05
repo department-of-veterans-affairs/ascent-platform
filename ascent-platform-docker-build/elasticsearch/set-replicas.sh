73f7f4b75c5081300cd999d900501665678cf0cf
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-05 13:24:48 -0600

    Env variables for es_pass and replica_amount. EnvConsul template change to get pass for es user

--- ascent-platform-docker-build/elasticsearch/set-replicas.sh
+++ ascent-platform-docker-build/elasticsearch/set-replicas.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+
+generate_replica_data() {
+  cat <<EOF
+{
+  "template" : "*",
+  "settings" : {"number_of_replicas" : "$REPLICA_AMOUNT" }
+}
+EOF
+}
+
+
+generate_newpass_data() {
+  cat << EOF
+{
+  "password" : "$ES_PASSWORD"
+}
+EOF
+}
+
+
+
+
 echo "--- Sleeping to wait for config of elasticsearch to complete"
 sleep 2m
-echo "--- setting number of replicas..."
+echo "--- changing default password and setting number of replicas..."
 
-curl -XPUT -u elastic:changeme 'localhost:9200/_template/all_index_template' -H 'Content-Type: application/json' -d ' 
-{ 
-  "template" : "*", 
-  "settings" : {"number_of_replicas" : $REPLICA_AMOUNT }
-} '
+curl -XPUT -u elastic:changeme 'localhost:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "$(generate_newpass_data)"
+
+
+
+curl -XPUT -u elastic:$ES_PASSWORD 'localhost:9200/_template/all_index_template' -H 'Content-Type: application/json' -d "$(generate_replica_data)"
+
