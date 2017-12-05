1839f5777cb5efe187b3218b2ee105a1e152541f
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-05 09:45:30 -0600

    clean up set-replicas, created env variable for amount of replicas to set

--- ascent-platform-docker-build/elasticsearch/set-replicas.sh
+++ ascent-platform-docker-build/elasticsearch/set-replicas.sh
@@ -2,17 +2,9 @@
 echo "--- Sleeping to wait for config of elasticsearch to complete"
 sleep 2m
 echo "--- setting number of replicas..."
-#curl -XPUT -u elastic:changeme 'localhost:9200/_all/_settings?pretty' -H 'Content-Type: application/json' -d'
-#{
-#    "index" : {
-#        "number_of_replicas" : 2
-#    }
-#}
-#'
 
-
-curl -XPUT -u elastic:changeme 'localhost:9200/_template/all_index_template' -d ' 
+curl -XPUT -u elastic:changeme 'localhost:9200/_template/all_index_template' -H 'Content-Type: application/json' -d ' 
 { 
   "template" : "*", 
-  "settings" : {"number_of_replicas" : 4 }
+  "settings" : {"number_of_replicas" : $REPLICA_AMOUNT }
 } '
