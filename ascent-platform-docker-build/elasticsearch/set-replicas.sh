54e7ad688c058546a567c47006ffae6ea222de33
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-06 17:54:59 -0600

    Took out unneeded env variables and added polling for availability of elasticsearch before making configuration changes.

--- ascent-platform-docker-build/elasticsearch/set-replicas.sh
+++ ascent-platform-docker-build/elasticsearch/set-replicas.sh
@@ -21,13 +21,14 @@ EOF
 
 
 
-echo "--- Sleeping to wait for config of elasticsearch to complete"
-sleep 2m
+echo "--- Polling to wait for config of elasticsearch to complete"
+until $(curl -XGET --output /dev/null --silent --head --fail -u elastic:$ES_PASSWORD localhost:9200/_cat/health); do
+    sleep 5
+done
+
+
 echo "--- changing default password and setting number of replicas..."
 
-curl -XPUT -u elastic:changeme 'localhost:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "$(generate_newpass_data)"
-
-
+curl -XPUT -u elastic:$ES_PASSWORD 'localhost:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "$(generate_newpass_data)"
 
 curl -XPUT -u elastic:$ES_PASSWORD 'localhost:9200/_template/all_index_template' -H 'Content-Type: application/json' -d "$(generate_replica_data)"
-
