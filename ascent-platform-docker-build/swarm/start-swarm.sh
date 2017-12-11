169f51bfb326658f8fb49ba03e5320584e33694e
Nate Paulus <npaulus@meetveracity.com>
2017-12-10 19:45:28 -0600

    Locking the boot2docker image to 17.11.0-ce
    Vault doesn't seem to be able to write to its volume in its container under 17.09.1-ce which is listed as the latest release on their website.

--- ascent-platform-docker-build/swarm/start-swarm.sh
+++ ascent-platform-docker-build/swarm/start-swarm.sh
@@ -2,9 +2,10 @@
 #Create 3 nodes for our swarm. One manager and two workers.
 docker-machine create --driver virtualbox \
   --virtualbox-memory 3072 \
+  --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v17.11.0-ce/boot2docker.iso \
   manager1
-docker-machine create --driver virtualbox --virtualbox-memory 3072 node1
-docker-machine create --driver virtualbox --virtualbox-memory 3072 node2
+docker-machine create --driver virtualbox --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v17.11.0-ce/boot2docker.iso --virtualbox-memory 3072 node1
+docker-machine create --driver virtualbox --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v17.11.0-ce/boot2docker.iso --virtualbox-memory 3072 node2
 
 docker-machine ssh manager1 "sudo sysctl -w vm.max_map_count=262144"
 docker-machine ssh node1 "sudo sysctl -w vm.max_map_count=262144"
