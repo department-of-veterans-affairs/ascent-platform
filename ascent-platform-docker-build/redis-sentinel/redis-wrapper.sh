ae08d0977497f887111b7a98234c7683ceda3d95
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-18 10:55:16 -0600

    put in actual key vault

--- ascent-platform-docker-build/redis-sentinel/redis-wrapper.sh
+++ ascent-platform-docker-build/redis-sentinel/redis-wrapper.sh
@@ -2,8 +2,8 @@
 
 # --- Remove all the sentinal stuff
 rm /redis/sentinel-wrapper.sh
-rm sentinel.conf
-rm sentinel-wrapper.sh
+rm /redis/sentinel.conf
 
-#echo "redis pass is: ${SPRING_REDIS_PASSWORD}"
+
+echo "redis pass is: ${SPRING_REDIS_PASSWORD}"
 redis-server --appendonly no --save "" --repl-diskless-sync yes --requirepass "${SPRING_REDIS_PASSWORD}" --masterauth "${SPRING_REDIS_PASSWORD}" "$@"
