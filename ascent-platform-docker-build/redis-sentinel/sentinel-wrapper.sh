b463ea690bb41c7d66fdbe0c3bc7ce15057ca44d
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-22 11:03:51 -0600

    changed hard coded pass into variable

--- ascent-platform-docker-build/redis-sentinel/sentinel-wrapper.sh
+++ ascent-platform-docker-build/redis-sentinel/sentinel-wrapper.sh
@@ -3,7 +3,7 @@
 sh /redis/configure-sentinel-conf.sh
 
 echo "polling for redis..."
-until redis-cli -a changeme -h redis-master -p 6379 PING | grep -m 1 "PONG"; 
+until redis-cli -a ${SPRING_REDIS_PASSWORD} -h redis-master -p 6379 PING | grep -m 1 "PONG"; 
 do 
   sleep 5 
   echo "didn't get PONG. Trying again."
