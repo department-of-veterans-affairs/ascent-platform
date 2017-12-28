9b27210d90e850dfce7a45535159c9fe84a77b70
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-28 08:36:35 -0600

    adding rabbitmq container scripts with docker-compose file

---
+++ ascent-platform-docker-build/rabbitmq/run-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ENVCONSUL_CONFIG="/etc/rabbitmq/template/envconsul-config.hcl"
+CMD=/docker-entrypoint.sh rabbitmq-server
+
+echo "--- polling to wait for vault"
+until $(curl -XGET --insecure --fail --output /dev/null --silent -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/application); do
+  echo "--trying again"
+  sleep 5
+done
+
+envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" -vault-token="$VAULT_TOKEN" $CMD "$@"
