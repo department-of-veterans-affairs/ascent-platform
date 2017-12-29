52b19661c2f65f5c3e855be9bf8587453202deb4
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-29 12:24:04 -0600

    add polling to wait for es-config to complete; uncomment consul-template command to populate ssl info.

--- ascent-platform-docker-build/kibana/run-wrapper.sh
+++ ascent-platform-docker-build/kibana/run-wrapper.sh
@@ -2,6 +2,7 @@
 
 ENVCONSUL_CONFIG="/usr/share/kibana/template/envconsul-config.hcl"
 CONSUL_TEMPLATE_CONFIG="/usr/share/kibana/template/consul-template-config.hcl"
+SSL_CONFIG="/usr/share/kibana/config/kibana.yml"
 BASE_CONFIG="/usr/share/kibana/template/kibana.yml"
 CMD="/usr/local/bin/kibana-docker"
 
@@ -15,9 +16,11 @@ fi
 
 cp $BASE_CONFIG /usr/share/kibana/config/kibana.yml
 
+/docker/wait-es-container-shutdown.sh
+
 # If ENVCONSUL_CONFIG is set then run under envconsul to provide secrets in env vars to the process
 if [[ $VAULT_TOKEN ]]; then
-#    consul-template -once -config="$CONSUL_TEMPLATE_CONFIG" -vault-addr="$VAULT_ADDR"
+    consul-template -once -config="$CONSUL_TEMPLATE_CONFIG" -vault-addr="$VAULT_ADDR"
     envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" $CMD "$@"
 else
     $CMD "$@"
