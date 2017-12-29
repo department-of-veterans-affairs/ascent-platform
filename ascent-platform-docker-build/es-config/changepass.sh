3d2f2cccae2d3bd764755259bf99383594c08b31
Elisabeth Sorrell <sorrelle04@gmail.com>
2017-12-29 12:06:05 -0600

    specify curl cert and https url in a variable

--- ascent-platform-docker-build/es-config/changepass.sh
+++ ascent-platform-docker-build/es-config/changepass.sh
@@ -8,9 +8,10 @@ generate_pass_data() {
 EOF
 }
 
+ES_URL="--cacert /usr/share/elasticsearch/config/ca.pem https://elastic.internal.vets-api.gov:9200/_xpack/security"
 echo "---- changing password"
 default_pass=changeme
-curl -XPOST -u elastic:$default_pass 'elasticsearch:9200/_xpack/security/user/elastic/_password?pretty' -H 'Content-Type: application/json' -d "$(generate_pass_data $ES_PASSWORD)"
+curl -XPOST -u elastic:$default_pass  $ES_URL/user/elastic/_password?pretty -H 'Content-Type: application/json' -d "$(generate_pass_data $ES_PASSWORD)"
 echo "---- changing kibana user pass"
-curl -XPOST -u elastic:$ES_PASSWORD 'elasticsearch:9200/_xpack/security/user/kibana/_password?pretty' -H 'Content-Type: application/json' -d "$(generate_pass_data $KIBANA_PASSWORD)"
+curl -XPOST -u elastic:$ES_PASSWORD $ES_URL/user/kibana/_password?pretty -H 'Content-Type: application/json' -d "$(generate_pass_data $KIBANA_PASSWORD)"
 
