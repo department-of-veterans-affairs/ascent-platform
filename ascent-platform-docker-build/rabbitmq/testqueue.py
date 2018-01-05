2700556cf055aee629f29ca84a20c77a480af858
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-01-05 09:19:56 -0600

    test by queueing a message

---
+++ ascent-platform-docker-build/rabbitmq/testqueue.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('guest', 'default')
+
+
+connectionParameters = pika.connection.ConnectionParameters('localhost', 5672, '/', credentials)
+connection = pika.BlockingConnection(connectionParameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+
+connection.close()
+
+
