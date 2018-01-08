c6c959a6645cdcbbceab0c26671d70385dc28982
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-01-08 09:24:14 -0600

    add use of set-url script to source

---
+++ ascent-platform-docker-build/es-config/set-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ -z $SECURE_CONNECT ]]; then
+    SECURE_CONNECT=false
+    echo "elastic.internal.vets-api.gov:9200" >> es-url
+fi
+
+if [ "$SECURE_CONNECT" = true ]; then
+    consul-template -once -config="$CONSUL_TEMPLATE_CONFIG" -vault-addr="$VAULT_ADDR" -vault-token="$VAULT_TOKEN"
+    echo "--cacert /usr/share/elasticsearch/config/ca.pem  https://elastic.internal.vets-api.gov:9200" >> es-url
+fi
+
