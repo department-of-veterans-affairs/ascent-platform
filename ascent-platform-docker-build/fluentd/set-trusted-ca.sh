b632ca97e719d22b9245c0d2126446f71fac0c4c
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-01-08 15:52:42 -0600

    delete checks for VAULT_TOKEN and SECURE_CoNNECt. already checked for before called

--- ascent-platform-docker-build/fluentd/set-trusted-ca.sh
+++ ascent-platform-docker-build/fluentd/set-trusted-ca.sh
@@ -6,12 +6,8 @@ mkdir /usr/local/share/ca-certificates/ascent
 echo "------- setting trusted ca..."
 echo "secure-connect: $SECURE_CONNECT"
 #Check if VAULT_TOKEN set and SECURE_CONNECT set to true
-if [[ -z $VAULT_TOKEN ]]; then
-  if [[ $SECURE_CONNECT ]]; then
-     echo "Secure: $SECURE_CONNECT"
-     echo "Downloading Vault CA certificate from $VAULT_ADDR/v1/pki/ca/pem"
-     curl -L -s --insecure $VAULT_ADDR/v1/pki/ca/pem > /usr/local/share/ca-certificates/ascent/vault-ca.crt
-     update-ca-certificates
-  fi
-fi
+echo "Secure: $SECURE_CONNECT"
+echo "Downloading Vault CA certificate from $VAULT_ADDR/v1/pki/ca/pem"
+curl -L -s --insecure $VAULT_ADDR/v1/pki/ca/pem > /usr/local/share/ca-certificates/ascent/vault-ca.crt
+update-ca-certificates
 
