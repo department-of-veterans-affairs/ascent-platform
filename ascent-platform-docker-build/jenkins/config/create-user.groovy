450f83a5c86c078b990b7bc63405cabc03ae93d1
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-07 14:35:29 -0600

    add script to create admin console user

---
+++ ascent-platform-docker-build/jenkins/config/create-user.groovy
@@ -0,0 +1,13 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("jenkins", "jenkins")
+instance.setSecurityRealm(hudsonRealm)
+def strategy = new GlobalMatrixAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, "jenkins")
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+
