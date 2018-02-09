70b9e6840c11ca5143101af4f7b5df3749444794
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-09 09:50:44 -0600

    add comment

--- ascent-platform-docker-build/jenkins/config/create-user.groovy
+++ ascent-platform-docker-build/jenkins/config/create-user.groovy
@@ -3,6 +3,7 @@ import hudson.security.*
 
 def instance = Jenkins.getInstance()
 
+// Create Jenkins user and add Administer privilege
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount("jenkins", "jenkins")
 instance.setSecurityRealm(hudsonRealm)
@@ -10,4 +11,3 @@ def strategy = new GlobalMatrixAuthorizationStrategy()
 strategy.add(Jenkins.ADMINISTER, "jenkins")
 instance.setAuthorizationStrategy(strategy)
 instance.save()
-
