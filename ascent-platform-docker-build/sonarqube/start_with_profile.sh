5b6d632772ec483701f9687b5f90f914ca5cb6ad
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-14 12:44:58 -0600

    comment out logic for setting profile. will move that to config-jenkins-sonar container.

--- ascent-platform-docker-build/sonarqube/start_with_profile.sh
+++ ascent-platform-docker-build/sonarqube/start_with_profile.sh
@@ -9,30 +9,33 @@
 
 ./bin/run.sh &
 
-function curlAdmin {
-    curl -v -u admin:admin $@
-}
-
-function isUp {
-    curl -s -u admin:admin -f "$BASE_URL/api/system/info"
-}
-
-# Wait for server to be up
-PING=`isUp`
-while [ -z "$PING" ]
-do
-    sleep 5
-    PING=`isUp`
-done
-
-# Restore qualityprofile and exclude Spring Boot Main Application.java class
-if [ "$LANGUAGE" ] && [ "$PROFILE_NAME" ]; then
-    curlAdmin -F "backup=@/qualityprofile/java-ascent-32413.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
-    curlAdmin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
-    curlAdmin -X POST "$BASE_URL/api/settings/set?key=sonar.coverage.exclusions&values=**/api/v*/transfer/**/*,**/*Application.java"
-fi
-
-# Create a token for admin user to authenticate to jenkins
-./provision/set_token.sh
-
+#function curlAdmin {
+#    curl -v -u admin:admin $@
+#}
+#
+#function isUp {
+#    curl -s -u admin:admin -f "$BASE_URL/api/system/info"
+#}
+#
+## Wait for server to be up
+#PING=`isUp`
+#while [ -z "$PING" ]
+#do
+#    sleep 5
+#    PING=`isUp`
+#done
+#
+## Restore qualityprofile and exclude Spring Boot Main Application.java class
+#if [ "$LANGUAGE" ] && [ "$PROFILE_NAME" ]; then
+#    curlAdmin -F "backup=@/qualityprofile/java-ascent-32413.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
+#    curlAdmin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
+#    curlAdmin -X POST "$BASE_URL/api/settings/set?key=sonar.coverage.exclusions&values=**/api/v*/transfer/**/*,**/*Application.java"
+#fi
+#
+## Set a jenkins webhook
+#./provision/set_jenkins_webhook.sh
+#
+## Create a token for admin user to authenticate to jenkins
+#./provision/set_token.sh
+#
 wait
