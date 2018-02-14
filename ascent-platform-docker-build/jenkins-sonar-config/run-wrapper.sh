af3902bc2a06d236ef6d953988bde621ce8c01d2
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-14 15:56:31 -0600

    add execution of setting sonar's main profile, jenkins webhook, and admin token

--- ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
+++ ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+ENV_CONSUL_CONFIG=/usr/share/configure/template/envconsul-config.hcl
+ENVCONSUL_CMD="envconsul -config $ENV_CONSUL_CONFIG -vault-addr=$VAULT_ADDR -vault-token=$VAULT_TOKEN"
+
 echo "---- POLLING ALL SERVICES UNTIL THEY'RE UP"
-/usr/share/configure/poll-services.sh
-tail -f /var/null
+$ENVCONSUL_CMD /usr/share/configure/poll-services.sh
+
+echo "---- SONAR: SETTING MAIN PROFILE"
+$ENVCONSUL_CMD /usr/share/configure/sonar/set_main_profile.sh
+
+echo "---- SONAR: CONFIGURING JENKINS WEBHOOK"
+$ENVCONSUL_CMD /usr/share/configure/sonar/set_jenkins_webhook.sh
+
+echo "---- SONAR: GENERATING AN admin USER TOKEN"
+$ENVCONSUL_CMD /usr/share/configure/sonar/set_token.sh
+
+echo "done!"
+tail -f /dev/null
