ff20c48f9982033025c84d6ac91b91a118b4b934
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-14 13:22:12 -0600

    renamed start_with_profile to run-wrapper

--- ascent-platform-docker-build/sonarqube/run-wrapper.sh
+++ ascent-platform-docker-build/sonarqube/run-wrapper.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#
+ENV_CONSUL_CONFIG=/opt/sonarqube/template/envconsul-config.hcl
+CMD=./provision/set_new_password.sh
 
 # Install Github plugin
-
 ./provision/install_github_plugin.sh
 
 # Start Sonar
-
 ./bin/run.sh &
 
-# Change admin password
-./provision/set_new_password.sh
+
+echo "Changing admin password..."
+if [[ $VAULT_TOKEN ]]; then
+   envconsul -config="$ENV_CONSUL_CONFIG" -vault-addr="$VAULT_ADDR" -vault-token="$VAULT_TOKEN" $CMD "$@"
+fi
+echo "Done!"
 
 #function curlAdmin {
 #    curl -v -u admin:admin $@
