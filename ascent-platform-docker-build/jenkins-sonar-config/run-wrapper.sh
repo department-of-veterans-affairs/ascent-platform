2d543a4ef0b4da6669d11bf1ffae0f6fba88bfee
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-15 11:42:49 -0600

    only make sonar config changes if the ascent profile has been set already

--- ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
+++ ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
@@ -5,14 +5,28 @@ ENVCONSUL_CMD="envconsul -config $ENV_CONSUL_CONFIG -vault-addr=$VAULT_ADDR -vau
 echo "---- POLLING ALL SERVICES UNTIL THEY'RE UP"
 $ENVCONSUL_CMD /usr/share/configure/poll-services.sh
 
-echo "---- SONAR: SETTING MAIN PROFILE"
-$ENVCONSUL_CMD /usr/share/configure/sonar/set_main_profile.sh
+profileExists=`$ENVCONSUL_CMD /usr/share/configure/sonar/test-if-ascent-profile-exists.sh`
 
-echo "---- SONAR: CONFIGURING JENKINS WEBHOOK"
-$ENVCONSUL_CMD /usr/share/configure/sonar/set_jenkins_webhook.sh
+if [ "$profileExists" = "FALSE" ]; then 
+  echo "---- SONAR: SETTING MAIN PROFILE"
+  $ENVCONSUL_CMD /usr/share/configure/sonar/set_main_profile.sh
+  
+  echo ""
+  echo "---- SONAR: CONFIGURING JENKINS WEBHOOK"
+  $ENVCONSUL_CMD /usr/share/configure/sonar/set_jenkins_webhook.sh
 
-echo "---- SONAR: GENERATING AN admin USER TOKEN"
-$ENVCONSUL_CMD /usr/share/configure/sonar/set_token.sh
+  echo "---- SONAR: GENERATING AN admin USER TOKEN"
+  $ENVCONSUL_CMD /usr/share/configure/sonar/set_token.sh
+else
+  echo "---- SONAR: profile already configured. WILL NOT configure anything"
+fi
 
+if [ "$CONFIGURE_JENKINS" = "true" ]; then
+   echo "## CONFIGURING JENKINS"
+else 
+   echo "## Jenkins set to NOT be configured."
+   echo "   If you want to change this, set the env variable"
+   echo "   CONFIGURE_JENKINS to true in your docker-compose file"
+fi
 echo "done!"
 tail -f /dev/null
