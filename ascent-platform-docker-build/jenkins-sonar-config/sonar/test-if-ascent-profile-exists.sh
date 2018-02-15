9328c628d3f1244d44f86f257286fff5342f2b22
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-15 11:44:09 -0600

    add script to test if ascent profile exists in sonar

---
+++ ascent-platform-docker-build/jenkins-sonar-config/sonar/test-if-ascent-profile-exists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Test for existence of ascent profile in sonarqube
+# echo TRUE if it exists, echo FALSE if it doesn't
+
+test=`curl -u admin:default $SONAR_URL/api/qualityprofiles/search? | grep "ASCENT"`
+
+if [ "$test" ]; then
+ echo "TRUE"
+else
+ echo "FALSE"
+fi
+
