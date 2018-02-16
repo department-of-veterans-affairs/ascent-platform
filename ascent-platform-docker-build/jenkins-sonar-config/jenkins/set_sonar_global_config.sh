f30aaf2bd5bf31e0808796625f0568f1e44c6f83
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-16 09:51:53 -0600

    add a comment

--- ascent-platform-docker-build/jenkins-sonar-config/jenkins/set_sonar_global_config.sh
+++ ascent-platform-docker-build/jenkins-sonar-config/jenkins/set_sonar_global_config.sh
@@ -7,4 +7,6 @@ CRUMB=`/usr/share/configure/jenkins/get-crumb.sh $API_TOKEN`
 echo "CRUMB=$CRUMB"
 
 # Hit the jenkins /scriptText endpoint to run the groovy script
+# Works the same way for update as well as creation, so creates if doesn't
+#   exist, and updates if id exists already
 curl -d "script=$(cat /usr/share/configure/jenkins/sonar_global_config.groovy)" -H $CRUMB -v --user $JENKINS_USERNAME:$API_TOKEN $JENKINS_URL/scriptText
