68879b70fd06b523dbb366f3af0a3a0f1e1e1109
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-02-21 00:20:44 -0500

    PLAT-42: Indentation and minor updates

--- ascent-platform-parent/ascent-platform-starter-aws/src/main/java/gov/va/ascent/starter/aws/autoconfigure/sqs/config/AbstractSqsConfiguration.java
+++ ascent-platform-parent/ascent-platform-starter-aws/src/main/java/gov/va/ascent/starter/aws/autoconfigure/sqs/config/AbstractSqsConfiguration.java
@@ -4,6 +4,7 @@ import com.amazon.sqs.javamessaging.AmazonSQSExtendedClient;
 import com.amazon.sqs.javamessaging.ExtendedClientConfiguration;
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazon.sqs.javamessaging.SQSSession;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -17,6 +18,9 @@ import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import javax.jms.ConnectionFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,134 +37,137 @@ import org.springframework.util.StringUtils;
 @EnableJms
 public abstract class AbstractSqsConfiguration {
 
-  @Bean
-  public abstract ConnectionFactory connectionFactory(SqsProperties sqsProperties);
+	@Bean
+	public abstract ConnectionFactory connectionFactory(SqsProperties sqsProperties);
 
-  @Bean
-  public DestinationResolver destinationResolver(SqsProperties sqsProperties) {
-    return new StaticDestinationResolver(sqsProperties.getQueueName());
-  }
+	@Bean
+	public DestinationResolver destinationResolver(SqsProperties sqsProperties) {
+		return new StaticDestinationResolver(sqsProperties.getQueueName());
+	}
 
-  @Bean
-  public JmsListenerContainerFactory<?> jmsListenerContainerFactory(
-      ConnectionFactory connectionFactory, DestinationResolver destinationResolver) {
+	@Bean
+	public JmsListenerContainerFactory<?> jmsListenerContainerFactory(
+			ConnectionFactory connectionFactory, DestinationResolver destinationResolver) {
 
-    DefaultJmsListenerContainerFactory jmsListenerContainerFactory =
-        new DefaultJmsListenerContainerFactory();
-    jmsListenerContainerFactory.setConnectionFactory(connectionFactory);
-    jmsListenerContainerFactory.setDestinationResolver(destinationResolver);
-    jmsListenerContainerFactory.setSessionTransacted(false);
+		DefaultJmsListenerContainerFactory jmsListenerContainerFactory =
+				new DefaultJmsListenerContainerFactory();
+		jmsListenerContainerFactory.setConnectionFactory(connectionFactory);
+		jmsListenerContainerFactory.setDestinationResolver(destinationResolver);
+		jmsListenerContainerFactory.setSessionTransacted(false);
+		jmsListenerContainerFactory.setSessionAcknowledgeMode(SQSSession.UNORDERED_ACKNOWLEDGE);
 
-    return jmsListenerContainerFactory;
-  }
+		return jmsListenerContainerFactory;
+	}
 
-  @Bean
-  public JmsTemplate jmsTemplate(
-      SqsProperties sqsProperties, ConnectionFactory connectionFactory) {
+	@Bean
+	public JmsTemplate jmsTemplate(
+			SqsProperties sqsProperties, ConnectionFactory connectionFactory) {
 
-    JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
-    jmsTemplate.setDefaultDestinationName(sqsProperties.getQueueName());
-    return jmsTemplate;
-  }
+		JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+		jmsTemplate.setDefaultDestinationName(sqsProperties.getQueueName());
+		jmsTemplate.setSessionAcknowledgeMode(SQSSession.UNORDERED_ACKNOWLEDGE);
+		jmsTemplate.setMessageTimestampEnabled(true);
+		return jmsTemplate;
+	}
 
-  protected SQSConnectionFactory createStandardSQSConnectionFactory(SqsProperties sqsProperties) {
-    AmazonSQS sqsClient = createAmazonSQSClient(sqsProperties);
+	protected SQSConnectionFactory createStandardSQSConnectionFactory(SqsProperties sqsProperties) {
+		AmazonSQS sqsClient = createAmazonSQSClient(sqsProperties);
 
-    ProviderConfiguration providerConfiguration = new ProviderConfiguration();
-    sqsProperties.getNumberOfMessagesToPrefetch()
-        .ifPresent(providerConfiguration::setNumberOfMessagesToPrefetch);
+		ProviderConfiguration providerConfiguration = new ProviderConfiguration();
+		sqsProperties.getNumberOfMessagesToPrefetch()
+		.ifPresent(providerConfiguration::setNumberOfMessagesToPrefetch);
 
-    return new SQSConnectionFactory(providerConfiguration, sqsClient);
-  }
+		return new SQSConnectionFactory(providerConfiguration, sqsClient);
+	}
 
-  protected SQSConnectionFactory createExtendedSQSConnectionFactory(SqsProperties sqsProperties) {
-    AmazonS3 s3Client = createAmazonS3Client(sqsProperties);
+	protected SQSConnectionFactory createExtendedSQSConnectionFactory(SqsProperties sqsProperties) {
+		AmazonS3 s3Client = createAmazonS3Client(sqsProperties);
 
-    // Set the SQS extended client configuration with large payload support enabled.
-    ExtendedClientConfiguration extendedClientConfig = new ExtendedClientConfiguration()
-        .withLargePayloadSupportEnabled(s3Client, sqsProperties.getExtended().getS3BucketName());
+		// Set the SQS extended client configuration with large payload support enabled.
+		ExtendedClientConfiguration extendedClientConfig = new ExtendedClientConfiguration()
+				.withLargePayloadSupportEnabled(s3Client, sqsProperties.getExtended().getS3BucketName());
 
-    ProviderConfiguration providerConfiguration = new ProviderConfiguration();
-    sqsProperties.getNumberOfMessagesToPrefetch()
-        .ifPresent(providerConfiguration::setNumberOfMessagesToPrefetch);
+		ProviderConfiguration providerConfiguration = new ProviderConfiguration();
+		sqsProperties.getNumberOfMessagesToPrefetch()
+		.ifPresent(providerConfiguration::setNumberOfMessagesToPrefetch);
 
-    AmazonSQS sqsClient = createAmazonSQSClient(sqsProperties);
+		AmazonSQS sqsClient = createAmazonSQSClient(sqsProperties);
 
-    return new SQSConnectionFactory(
-        providerConfiguration,
-        new AmazonSQSExtendedClient(sqsClient, extendedClientConfig)
-    );
-  }
+		return new SQSConnectionFactory(
+				providerConfiguration,
+				new AmazonSQSExtendedClient(sqsClient, extendedClientConfig)
+				);
+	}
 
-  private AmazonS3 createAmazonS3Client(SqsProperties sqsProperties) {
-    AWSCredentialsProvider awsCredentialsProvider = createAwsCredentialsProvider(
-        sqsProperties.getAccessKey(),
-        sqsProperties.getSecretKey()
-    );
+	private AmazonS3 createAmazonS3Client(SqsProperties sqsProperties) {
+		AWSCredentialsProvider awsCredentialsProvider = createAwsCredentialsProvider(
+				sqsProperties.getAccessKey(),
+				sqsProperties.getSecretKey()
+				);
 
-    Regions region = Regions.fromName(sqsProperties.getRegion());
+		Regions region = Regions.fromName(sqsProperties.getRegion());
 
-    AmazonS3 amazonS3Client = AmazonS3ClientBuilder
-        .standard()
-        .withCredentials(awsCredentialsProvider)
-        .withRegion(region)
-        .build();
+		AmazonS3 amazonS3Client = AmazonS3ClientBuilder
+				.standard()
+				.withCredentials(awsCredentialsProvider)
+				.withRegion(region)
+				.build();
 
-    String s3BucketName = sqsProperties.getExtended().getS3BucketName();
+		String s3BucketName = sqsProperties.getExtended().getS3BucketName();
 
-    if (!amazonS3Client.doesBucketExist(s3BucketName)) {
-      amazonS3Client.createBucket(s3BucketName);
+		if (!amazonS3Client.doesBucketExist(s3BucketName)) {
+			amazonS3Client.createBucket(s3BucketName);
 
-      // Set the Amazon S3 bucket name, and set a lifecycle rule on the bucket to
-      // permanently delete objects a certain number of days after each object's creation date.
-      // Next, create the bucket, and enable message objects to be stored in the bucket.
-      BucketLifecycleConfiguration.Rule expirationRule =
-          new BucketLifecycleConfiguration.Rule()
-              .withExpirationInDays(14).withStatus("Enabled");
+			// Set the Amazon S3 bucket name, and set a lifecycle rule on the bucket to
+			// permanently delete objects a certain number of days after each object's creation date.
+			// Next, create the bucket, and enable message objects to be stored in the bucket.
+			BucketLifecycleConfiguration.Rule expirationRule =
+					new BucketLifecycleConfiguration.Rule()
+					.withExpirationInDays(14).withStatus("Enabled");
 
-      BucketLifecycleConfiguration lifecycleConfig =
-          new BucketLifecycleConfiguration().withRules(expirationRule);
+			BucketLifecycleConfiguration lifecycleConfig =
+					new BucketLifecycleConfiguration().withRules(expirationRule);
 
-      amazonS3Client.setBucketLifecycleConfiguration(s3BucketName, lifecycleConfig);
-    }
+			amazonS3Client.setBucketLifecycleConfiguration(s3BucketName, lifecycleConfig);
+		}
 
-    return amazonS3Client;
-  }
+		return amazonS3Client;
+	}
 
-  private AmazonSQS createAmazonSQSClient(SqsProperties sqsProperties) {
-    Regions region = Regions.fromName(sqsProperties.getRegion());
+	private AmazonSQS createAmazonSQSClient(SqsProperties sqsProperties) {
+		Regions region = Regions.fromName(sqsProperties.getRegion());
 
-    EndpointConfiguration endpointConfiguration = new EndpointConfiguration(
-        sqsProperties.getEndpoint(), region.getName());
+		EndpointConfiguration endpointConfiguration = new EndpointConfiguration(
+				sqsProperties.getEndpoint(), region.getName());
 
-    AWSCredentialsProvider awsCredentialsProvider = createAwsCredentialsProvider(
-        sqsProperties.getAccessKey(),
-        sqsProperties.getSecretKey()
-    );
+		AWSCredentialsProvider awsCredentialsProvider = createAwsCredentialsProvider(
+				sqsProperties.getAccessKey(),
+				sqsProperties.getSecretKey()
+				);
 
-    return AmazonSQSClientBuilder
-        .standard()
-        .withCredentials(awsCredentialsProvider)
-        .withEndpointConfiguration(endpointConfiguration)
-        .build();
-  }
+		return AmazonSQSClientBuilder
+				.standard()
+				.withCredentials(awsCredentialsProvider)
+				.withEndpointConfiguration(endpointConfiguration)
+				.build();
+	}
 
-  private AWSCredentialsProvider createAwsCredentialsProvider(
-      String localAccessKey, String localSecretKey) {
+	private AWSCredentialsProvider createAwsCredentialsProvider(
+			String localAccessKey, String localSecretKey) {
 
-    AWSCredentialsProvider ec2ContainerCredentialsProvider =
-        new EC2ContainerCredentialsProviderWrapper();
+		AWSCredentialsProvider ec2ContainerCredentialsProvider =
+				new EC2ContainerCredentialsProviderWrapper();
 
-    if (StringUtils.isEmpty(localAccessKey) || StringUtils.isEmpty(localSecretKey)) {
-      return ec2ContainerCredentialsProvider;
-    }
+		if (StringUtils.isEmpty(localAccessKey) || StringUtils.isEmpty(localSecretKey)) {
+			return ec2ContainerCredentialsProvider;
+		}
 
-    AWSCredentialsProvider localAwsCredentialsProvider =
-        new AWSStaticCredentialsProvider(
-            new BasicAWSCredentials(localAccessKey, localSecretKey));
+		AWSCredentialsProvider localAwsCredentialsProvider =
+				new AWSStaticCredentialsProvider(
+						new BasicAWSCredentials(localAccessKey, localSecretKey));
 
-    return new AWSCredentialsProviderChain(
-        localAwsCredentialsProvider, ec2ContainerCredentialsProvider);
-  }
+		return new AWSCredentialsProviderChain(
+				localAwsCredentialsProvider, ec2ContainerCredentialsProvider);
+	}
 
 }
