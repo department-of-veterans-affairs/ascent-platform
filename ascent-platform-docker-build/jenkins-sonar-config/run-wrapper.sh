780e9440c2d295309621c987bab8772933bd2c46
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-21 12:03:38 -0600

    take out the CONFIGURE_JENKINS logic

--- ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
+++ ascent-platform-docker-build/jenkins-sonar-config/run-wrapper.sh
@@ -15,23 +15,17 @@ $ENVCONSUL_CMD $SONAR_DIR/set_jenkins_webhook.sh
 echo "---- SONAR: GENERATING AN admin USER TOKEN"
 $ENVCONSUL_CMD $SONAR_DIR/set_token.sh
 
-if [ "$CONFIGURE_JENKINS" = "true" ]; then
-   echo "## CONFIGURING JENKINS"
-   echo "---- JENKINS: SET SONAR TOKEN"
-   $ENVCONSUL_CMD $JENKINS_DIR/set_sonar_global_config.sh
-   
-   echo "---- JENKINS: SETTING NEXUS CREDENTIALS"
-   $ENVCONSUL_CMD $JENKINS_DIR/nexus_credentials.sh   
-   
-   echo "---- JENKINS: SETTING VAULT CREDENTIALS"
-   $ENVCONSUL_CMD $JENKINS_DIR/vault_credentials.sh
+echo "## CONFIGURING JENKINS"
+echo "---- JENKINS: SET SONAR TOKEN"
+$ENVCONSUL_CMD $JENKINS_DIR/set_sonar_global_config.sh
+
+echo "---- JENKINS: SETTING NEXUS CREDENTIALS"
+$ENVCONSUL_CMD $JENKINS_DIR/nexus_credentials.sh
+
+echo "---- JENKINS: SETTING VAULT CREDENTIALS"
+$ENVCONSUL_CMD $JENKINS_DIR/vault_credentials.sh
  
-   echo "---- JENKINS: SETTING GITHUB CREDENTIALS"
-   $ENVCONSUL_CMD $JENKINS_DIR/github_credentials.sh
-else 
-   echo "## Jenkins set to NOT be configured."
-   echo "   If you want to change this, set the env variable"
-   echo "   CONFIGURE_JENKINS to true in your docker-compose file"
-fi
+echo "---- JENKINS: SETTING GITHUB CREDENTIALS"
+$ENVCONSUL_CMD $JENKINS_DIR/github_credentials.sh
 echo "done!"
 
