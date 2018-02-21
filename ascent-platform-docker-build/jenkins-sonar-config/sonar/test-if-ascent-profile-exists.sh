d0796e39d8934c645c254d91533ee991cd1bb91a
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-21 11:43:31 -0600

    add sonar_password env variable

--- ascent-platform-docker-build/jenkins-sonar-config/sonar/test-if-ascent-profile-exists.sh
+++ ascent-platform-docker-build/jenkins-sonar-config/sonar/test-if-ascent-profile-exists.sh
@@ -3,7 +3,7 @@
 # Test for existence of ascent profile in sonarqube
 # echo TRUE if it exists, echo FALSE if it doesn't
 
-test=`curl -u admin:default $SONAR_URL/api/qualityprofiles/search? | grep "ASCENT"`
+test=`curl -u admin:$SONAR_PASSWORD $SONAR_URL/api/qualityprofiles/search? | grep "ASCENT"`
 
 if [ "$test" ]; then
  echo "TRUE"
