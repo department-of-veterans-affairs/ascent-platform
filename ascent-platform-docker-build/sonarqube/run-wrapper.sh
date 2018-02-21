e607895c6d5ec4ff7dc9aaf69e8505ad0979b0f9
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-21 16:54:20 -0600

    add sed statement to run-wrapper to have it change an environment var name

--- ascent-platform-docker-build/sonarqube/run-wrapper.sh
+++ ascent-platform-docker-build/sonarqube/run-wrapper.sh
@@ -5,20 +5,38 @@ ENV_CONSUL_CMD="envconsul -config $ENV_CONSUL_CONFIG -vault-addr=$VAULT_ADDR -va
 # Install Github plugin
 ./provision/install_github_plugin.sh
 
-# Start Sonar
-./bin/run.sh &
+
+if [[ $VAULT_TOKEN ]]; then
+   # poll for vault
+   echo "--- polling to wait for vault"
+   until $(curl -XGET --insecure --fail --output /dev/null --silent -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/sys/health); do
+     echo "--trying again"
+     sleep 5
+   done
+ 
+   # Change the jdbc env variable to have USER instead of USERNAME
+   # to match what's going to come from envconsul
+   sed -i 's/USERNAME/USER/g' ./bin/run.sh 
+
+   # Start Sonar
+   $ENV_CONSUL_CMD ./bin/run.sh &
+else 
+   ./bin/run.sh &
+fi
 
 # Wait for sonar to be up
 ./provision/wait_for_sonar.sh
 
-# TODO: add polling for vault availability
 echo "Changing admin password..."
 if [[ $VAULT_TOKEN ]]; then
    echo "Changing admin password..."
    $ENV_CONSUL_CMD ./provision/set_new_password.sh
    echo "Setting ascent profile..."
    $ENV_CONSUL_CMD ./provision/set_main_profile.sh
-   echo "Done!"
+else 
+   export SONAR_PASSWORD=admin
+   ./provision/set_main_profile.sh
 fi
 
+echo "Done!"
 wait
