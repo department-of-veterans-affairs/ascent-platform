3768d9d50aa8abc3e13b8e0b4c58c98dabfe4822
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-21 12:26:03 -0600

    add wait_for_sonar and execute set_main_profile

--- ascent-platform-docker-build/sonarqube/run-wrapper.sh
+++ ascent-platform-docker-build/sonarqube/run-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ENV_CONSUL_CONFIG=/opt/sonarqube/template/envconsul-config.hcl
-CMD=./provision/set_new_password.sh
+ENV_CONSUL_CMD="envconsul -config $ENV_CONSUL_CONFIG -vault-addr=$VAULT_ADDR -vault-token=$VAULT_TOKEN"
 
 # Install Github plugin
 ./provision/install_github_plugin.sh
@@ -8,9 +8,14 @@ CMD=./provision/set_new_password.sh
 # Start Sonar
 ./bin/run.sh &
 
+# Wait for sonar to be up
+./provision/wait_for_sonar.sh
+
 if [[ $VAULT_TOKEN ]]; then
    echo "Changing admin password..."
-   envconsul -config="$ENV_CONSUL_CONFIG" -vault-addr="$VAULT_ADDR" -vault-token="$VAULT_TOKEN" $CMD "$@"
+   $ENV_CONSUL_CMD ./provision/set_new_password.sh
+   echo "Setting ascent profile..."
+   $ENV_CONSUL_CMD ./provision/set_main_profile.sh
    echo "Done!"
 fi
 
