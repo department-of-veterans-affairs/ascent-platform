d415de746a871b52ad3b7c4ae3fd76a965ef2aaf
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-02-23 11:04:38 -0600

    change start command, have sed replace the create database so that it makes the character encoding utf-8, have it hang forever because it's now the entrypoint

--- ascent-platform-docker-build/sonarqube/database/run-wrapper.sh
+++ ascent-platform-docker-build/sonarqube/database/run-wrapper.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ENVCONSUL_CONFIG="/template/envconsul-config.hcl"
-CMD="/docker-entrypoint.sh postgres"
+CMD="/opt/cpm/bin/start.sh"
+
+# Have the sql init script set theg
+# database encoding to UTF8 once run
+SETUP_SCRIPT="/opt/cpm/bin/setup.sql"
+sed -i 's|create database PG_DATABASE|create database PG_DATABASE with encoding "UTF8" template=template0|g' $SETUP_SCRIPT
 
 if [[ $VAULT_TOKEN ]]; then
   envconsul -config="$ENVCONSUL_CONFIG" -vault-addr="$VAULT_ADDR" -vault-token="$VAULT_TOKEN" $CMD "$@"
@@ -10,4 +15,5 @@ else
   $CMD "$@"
 fi
 
+tail -f /dev/null
 wait
