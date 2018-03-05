3a26090e14cb4a60a88c9244fbf6a773fab18720
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-03-05 11:37:15 -0600

    add ip_list variable set to other locations

--- ascent-platform-docker-build/sonarqube/provision/db_cluster_wait.sh
+++ ascent-platform-docker-build/sonarqube/provision/db_cluster_wait.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Check for the existence of $DISCOVERY_SERVICE and 
-#    skip to just polling db (set ip_list to 'db')if not found
+#    skip to just polling db (set ip_list to 'db'
+#    and initial cluster size to 1) if not found
 if [ -z "$DISCOVERY_SERVICE" ]; then
-  ip_list="db"
   INITIAL_CLUSTER_SIZE=1
+  ip_list="db"
 else
   echo "--- polling for existence of cluster $CLUSTER_NAME in discovery service $DISCOVERY_SERVICE"
   until $(curl -XGET --fail --output /dev/null --silent --head http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/); do
@@ -16,13 +17,13 @@ else
     sleep 5
   done
   echo "--- cluster initialized. checking for nodes"
-  ip_list=`curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | tac | tac | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}'`
 fi
 
 keepgoing=true
 while [ "$keepgoing" = "true" ]; do
    if [ -z "$ip_list" ]; then
       echo "did not find nodes in cluster. trying again"
+      ip_list=`curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | tac | tac | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}'`
       sleep 5
    else
       echo "ip_list=$ip_list"
@@ -30,6 +31,7 @@ while [ "$keepgoing" = "true" ]; do
       echo "node_amount=$node_amount"
       if [[ "$node_amount" -lt "$INITIAL_CLUSTER_SIZE" ]]; then
          echo "node amount is less than $INITIAL_CLUSTER_SIZE. Trying again"
+         ip_list=`curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | tac | tac | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}'`
          sleep 5
       else 
          for i in $ip_list
