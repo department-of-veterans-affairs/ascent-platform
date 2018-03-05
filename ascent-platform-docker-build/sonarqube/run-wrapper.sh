a996a5ad613a193bcfdae2e0b215e42baa1e03b4
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-03-05 10:15:37 -0600

    put wait for db logic in the if statement to be run with envconsul command or not depending on  vault_token available

--- ascent-platform-docker-build/sonarqube/run-wrapper.sh
+++ ascent-platform-docker-build/sonarqube/run-wrapper.sh
@@ -5,12 +5,12 @@ ENV_CONSUL_CMD="envconsul -config $ENV_CONSUL_CONFIG -vault-addr=$VAULT_ADDR -va
 # Install Github plugin
 ./provision/install_github_plugin.sh
 
-./provision/db_cluster_wait.sh
-
 if [[ $VAULT_TOKEN ]]; then
    # poll for vault
    echo "--- polling to wait for vault"
    until $(curl -XGET --insecure --fail --output /dev/null --silent -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/sys/health); do
+     echo "output of curl for vault"
+     curl -XGET --insecure --fail --output /dev/null --silent -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/sys/health
      echo "--trying again"
      sleep 5
    done
@@ -18,10 +18,14 @@ if [[ $VAULT_TOKEN ]]; then
    # Change the jdbc env variable to have USER instead of USERNAME
    # to match what's going to come from envconsul
    sed -i 's/USERNAME/USER/g' ./bin/run.sh 
-
+  
+   # Wait for database to be available
+   $ENV_CONSUL_CMD ./provision/db_cluster_wait.sh
+    
    # Start Sonar
    $ENV_CONSUL_CMD ./bin/run.sh &
 else 
+   ./provision/db_cluster_wait.sh
    ./bin/run.sh &
 fi
 
