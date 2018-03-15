33ae277a88c0d0f841179ef440257fa5b6a78949
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-03-15 13:45:07 -0400

    PLAT-100: Updated exclusion list to add AbstractRemoteServiceCallMock

--- ascent-platform-docker-build/containers/sonarqube/provision/set_main_profile.sh
+++ ascent-platform-docker-build/containers/sonarqube/provision/set_main_profile.sh
@@ -9,6 +9,6 @@ SONAR_URL="http://localhost:9000"
 if [ "$LANGUAGE" ] && [ "$PROFILE_NAME" ]; then
     curlAdmin -F "backup=@/opt/sonarqube/provision/qualityprofile/java-ascent-32413.xml" -X POST "$SONAR_URL/api/qualityprofiles/restore"
     curlAdmin -X POST "$SONAR_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
-    curlAdmin -X POST "$SONAR_URL/api/settings/set?key=sonar.coverage.exclusions&values=**/api/v*/transfer/**/*,**/*Application.java"
+    curlAdmin -X POST "$SONAR_URL/api/settings/set?key=sonar.coverage.exclusions&values=**/api/v*/transfer/**/*,**/*Application.java,**/*AbstractRemoteServiceCallMock.java"
 fi
 
