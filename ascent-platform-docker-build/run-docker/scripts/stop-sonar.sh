ac7dc65b3005724dd0f57ca3839277ca71f1f300
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-03-15 15:45:01 -0500

    PLAT-100 take out --rmi all and -v

--- ascent-platform-docker-build/run-docker/scripts/stop-sonar.sh
+++ ascent-platform-docker-build/run-docker/scripts/stop-sonar.sh
@@ -4,4 +4,4 @@
 #   Can add additional args when starting up 
 #   like `stop-sonar.sh -v` to remove volumes
 docker-compose -f ../containers/sonarqube/docker-compose.yml \
-    down --rmi=all "$@"
+    down "$@"
