06ba1149a6dcf16cad65e3ba9ca9fe50b2e87704
jasonluck <jluck@meetveracity.com>
2018-06-04 15:53:43 -0400

    Adding creation of keystores.

--- ascent-platform-docker-build/containers/ascent-base/run-wrapper.sh
+++ ascent-platform-docker-build/containers/ascent-base/run-wrapper.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-CMD="java -Xms64m -Xmx256m -jar $JAR_FILE"
+if [[ -z $CMD ]]; then
+    CMD="java -Xms64m -Xmx256m -jar $JAR_FILE"
+fi
+
+if [[ -z $APP_NAME ]]; then
+    echo 'APP_NAME environment variable must be set when launching the container. This is needed to build the client SSL certificate keystore.'
+fi
 
 export INSTANCE_HOST_NAME=$(hostname)
 
@@ -18,6 +24,25 @@ if [[ $VAULT_TOKEN && $VAULT_ADDR ]]; then
     update-ca-certificates
     keytool -importcert -alias vault -keystore $JAVA_HOME/jre/lib/security/cacerts -noprompt -storepass changeit -file /usr/local/share/ca-certificates/ascent/vault-ca.crt
     
+    #Build the trusted keystore
+    CA_CERTS=$(curl -L -s --insecure -X LIST -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/ssl/trusted | jq -r '.data.keys[]')
+    for cert in $CA_CERTS; do
+        curl -L -s --insecure -X GET -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/ssl/trusted/$cert | jq -r '.data.certificate' > $TMPDIR/$cert.crt
+        keytool -importcert -alias $cert -keystore $JAVA_HOME/jre/lib/security/cacerts -noprompt -storepass changeit -file $TMPDIR/$cert.crt
+    done
+
+    #Build the client keystore
+    CLIENT_KEYSTORE_PASS='changeit'
+    keytool -genkey -alias app -keystore $JAVA_HOME/jre/lib/security/client.jks -storepass $CLIENT_KEYSTORE_PASS
+    keytool -delete -alias app -keystore $JAVA_HOME/jre/lib/security/client.jks -storepass $CLIENT_KEYSTORE_PASS
+    CLIENT_CERTS=$(curl -L -s --insecure -X LIST -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/ssl/client/$APP_NAME | jq -r '.data.keys[]')
+    for cert in $CLIENT_CERTS; do
+        curl -L -s --insecure -X GET -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/ssl/client/$APP_NAME/$cert | jq -r '.data.certificate' > $TMPDIR/$APP_NAME-$cert.crt
+        curl -L -s --insecure -X GET -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/ssl/client/$APP_NAME/$cert | jq -r '.data.private_key' > $TMPDIR/$APP_NAME-$cert.key
+        
+        openssl pkcs12 -export -out $TMPDIR/$APP_NAME-$cert.p12 -inkey $TMPDIR/$APP_NAME-$cert.key -in $TMPDIR/$APP_NAME-$cert.crt
+        keytool -importkeystore -srckeystore $TMPDIR/$APP_NAME-$cert.p12 -srcstoretype PKCS12 -destkeystore $JAVA_HOME/jre/lib/security/client.jks -deststoretype JKS -destkeypass $CLIENT_KEYSTORE_PASS
+    done
 
     #Launch the app in another shell to keep secrets secure
     envconsul -config="$ENVCONSUL_CONFIG" -vault-addr=$VAULT_ADDR $CMD
