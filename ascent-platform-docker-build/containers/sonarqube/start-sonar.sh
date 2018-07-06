2a682507ab7e95bb1986e3422d43495b55349049
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-06 14:15:56 -0500

    Simplify the sonarqube and db containers and get rid of clustering. We're not going to be using them in the new account, and it will be for dev use only

--- ascent-platform-docker-build/containers/sonarqube/start-sonar.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#Starts sonar cube
-docker-compose -f ./docker-compose_local/docker-compose.yml ../sonar-database/docker-compose_local/docker-compose.yml up --build -d
--- ascent-platform-docker-build/containers/sonarqube/stop-sonar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#Stops sonar cube
-#   Can add additional args when starting up 
-#   like `stop-sonar.sh -v` to remove volumes
-docker-compose -f docker-compose_local/docker-compose.yml \
-        -f ../sonarqube-database/docker-compose_local/docker-compose.yml \
-	down --rmi=all "$@"
