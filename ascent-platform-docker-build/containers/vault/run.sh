9e606f5e84853d78f6db4250d9136b3ed0e6352f
jasonluck <jluck@meetveracity.com>
2018-08-23 13:36:17 -0400

    fixing ttl for certs

--- ascent-platform-docker-build/containers/vault/run.sh
+++ ascent-platform-docker-build/containers/vault/run.sh
@@ -83,9 +83,10 @@ fi
 
 # Configure PKI backend
 echo "Configuring Vault PKI backend..."
-vault mount pki 
+vault mount pki
+vault mount-tune -max-lease-ttl=8760h pki 
 vault write pki/root/generate/internal common_name=internal.vetservices.gov
-vault write pki/roles/vetservices allow_any_name=true
+vault write pki/roles/vetservices allow_any_name=true max_ttl=72h
 echo "Vault PKI Backend successfully configured."
 
 # docker healthcheck
