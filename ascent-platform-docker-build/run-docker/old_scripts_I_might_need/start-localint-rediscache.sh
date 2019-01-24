78cefd798aa0b6d4959165b98eb1380863df44cb
Elisabeth Sorrell <sorrelle04@gmail.com>
2019-01-24 11:38:41 -0600

    Cleanup the ascent-platform-docker-build directory

--- ascent-platform-docker-build/run-docker/old_scripts_I_might_need/start-localint-rediscache.sh
+++
@@ -1,2 +0,0 @@
-# Start REDIS cache for local-int profile, running on localhost:6379
-docker-compose -f docker-compose.localint.cache.yml up --build -d
--- ascent-platform-docker-build/run-docker/scripts/stop-sonar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#Stops sonar cube
-#   Can add additional args when starting up 
-#   like `stop-sonar.sh -v` to remove volumes
-docker-compose -f ../containers/sonarqube/docker-compose.yml \
-    down "$@"
